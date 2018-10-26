cedbdfa60c97b8bdffea8ee0aff28258a7f9a2f3
Morgan Newman <mpnewman95@gmail.com>
2018-10-26 11:39:13 -0700

    Removes stale tests

--- src/components/workbench/tests/Workbench.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Workbench } from '../Workbench'
-import { shallow } from 'enzyme'
-
-describe('Workbench', () => {
-  let wrapper
-  // beforeEach(() => { wrapper = shallow(<Workbench />) });
-  it('Should render without crashing', () => {})
-})
